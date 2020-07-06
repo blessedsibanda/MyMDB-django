631e4a56f7e87ca42ab1f286f6a83a9948ca93f1
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2020-07-06 09:51:53 +0200

    add django-debug-toolbar

--- django/config/settings.py
+++ django/config/settings.py
@@ -38,9 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd party
+    'django_extensions',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Django Debug Toolbar
+INTERNAL_IPS = ['127.0.0.1']
